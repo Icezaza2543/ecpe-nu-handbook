834d37214e18219977ef5567355141b0193c7752
Terasit Juntarasombut <icezaza.jar@gmail.com>
2026-06-12 17:22:10 +0700

    chore: remove obsolete Python maintenance and utility scripts

--- list_ids.py
+++
@@ -1,8 +0,0 @@
-import json
-with open("src/data/studyPlan.ts", "r", encoding="utf-8") as f:
-    text = f.read()
-
-import re
-matches = re.findall(r'"courseId":\s*"(year[12]-sem[12]-.*?)"', text)
-for m in matches:
-    print(m)
